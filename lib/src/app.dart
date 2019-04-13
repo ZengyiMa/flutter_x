802d33243169a3c6429c12b2d59f3b6a886a2a1c
mazengyi <mazy@ctrip.com>
2019-04-14 00:17:45 +0800

    change home to widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  const App({Key key, this.title, this.home}) : super(key: key);
+  const App({Key key, this.title, this.widget}) : super(key: key);
 
   final String title;
-  final Widget home;
+  final Widget widget;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
-      home: home,
+      home: widget,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
