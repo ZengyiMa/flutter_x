7b0699fc1be06d1cc205ea7514f56f7deab5b78b
ZengyiMa <m.zengyi@gmail.com>
2019-04-14 01:37:25 +0800

    update Example Title

--- example/flutter_x_example/lib/main.dart
+++ example/flutter_x_example/lib/main.dart
@@ -24,7 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      title: "Flutter-x Demo",
+      title: "Flutter-x example",
     );
   }
 }
