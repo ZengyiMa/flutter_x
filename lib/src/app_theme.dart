f8f932824de3571296c5c2a31eee2521365f31d0
ZengyiMa <m.zengyi@gmail.com>
2019-04-20 00:26:12 +0800

    AppTheme Add FX prefix

--- lib/src/app_theme.dart
+++ lib/src/app_theme.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/foundation.dart';
 
-class AppTheme extends Diagnosticable {
-  const AppTheme({this.primaryColor, this.navigationBarBackgroundColor, this.titleStyle});
+class FXAppTheme extends Diagnosticable {
+  const FXAppTheme({this.primaryColor, this.navigationBarBackgroundColor, this.titleStyle});
   
   final Color primaryColor;
   final Color navigationBarBackgroundColor;
