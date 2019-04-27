d7e227f30bde998cf3fa6b9990236c686d67aace
ZengyiMa <m.zengyi@gmail.com>
2019-04-27 19:15:54 +0800

    Update navigationbar.dart
    support TextStyle 、hiddenBottom

--- lib/src/navigationbar.dart
+++ lib/src/navigationbar.dart
@@ -3,35 +3,48 @@ import 'package:flutter/cupertino.dart';
 import 'platform.dart';
 
 class FXNavigationBar extends StatelessWidget {
-  const FXNavigationBar({Key key, this.title, this.leading, this.actions, this.backgroundColor}) : super(key: key);
+  const FXNavigationBar(
+      {Key key,
+      this.title,
+      this.titleTextStyle,
+      this.leading,
+      this.actions,
+      this.backgroundColor,
+      this.hiddenBottomEffect})
+      : super(key: key);
 
   final String title;
+  final TextStyle titleTextStyle;
   final List<Widget> actions;
   final Widget leading;
   final Color backgroundColor;
+  final bool hiddenBottomEffect;
 
   @override
   Widget build(BuildContext context) {
-     if (FXPlatform.style() == FXAppStyle.material) {
+    if (FXPlatform.style() == FXAppStyle.material) {
       return AppBar(
-        title: Text(this.title),
+        title: Text(this.title, style: this.titleTextStyle),
         actions: this.actions,
         leading: leading,
         backgroundColor: this.backgroundColor,
+        elevation: hiddenBottomEffect == false ? 0 : null,
       );
     } else if (FXPlatform.style() == FXAppStyle.cupertino) {
       return CupertinoNavigationBar(
-          middle: Text(this.title),
-          trailing: this.actions != null ? Row(
-            mainAxisSize: MainAxisSize.min,
-            children: this.actions,
-          ) : null,
-          leading: this.leading,
-          backgroundColor: this.backgroundColor,
-        );
+        middle: Text(this.title, style: this.titleTextStyle),
+        trailing: this.actions != null
+            ? Row(
+                mainAxisSize: MainAxisSize.min,
+                children: this.actions,
+              )
+            : null,
+        leading: this.leading,
+        backgroundColor: this.backgroundColor,
+        border: hiddenBottomEffect == false ? null : Border(bottom: null),
+      );
     } else {
       return null;
     }
-   
   }
 }
