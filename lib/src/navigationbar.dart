519452025a9a76588d5e9681b9eb2960d78feba8
ZengyiMa <m.zengyi@gmail.com>
2019-04-27 18:50:01 +0800

    NavigationBar improve
    support Action And leading

--- lib/src/navigationbar.dart
+++ lib/src/navigationbar.dart
@@ -3,19 +3,28 @@ import 'package:flutter/cupertino.dart';
 import 'platform.dart';
 
 class FXNavigationBar extends StatelessWidget {
-  const FXNavigationBar({Key key, this.title}) : super(key: key);
+  const FXNavigationBar({Key key, this.title, this.leading, this.actions}) : super(key: key);
 
   final String title;
+  final List<Widget> actions;
+  final Widget leading;
 
   @override
   Widget build(BuildContext context) {
      if (FXPlatform.style() == FXAppStyle.material) {
       return AppBar(
-        title: Text(this.title)
+        title: Text(this.title),
+        actions: this.actions,
+        leading: leading,
       );
     } else if (FXPlatform.style() == FXAppStyle.cupertino) {
       return CupertinoNavigationBar(
           middle: Text(this.title),
+          trailing: this.actions != null ? Row(
+            mainAxisSize: MainAxisSize.min,
+            children: this.actions,
+          ) : null,
+          leading: this.leading,
         );
     } else {
       return null;
