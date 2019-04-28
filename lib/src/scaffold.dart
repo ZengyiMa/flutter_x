50936a4d780078e81b783a11ad8854d5a8e68a26
ZengyiMa <m.zengyi@gmail.com>
2019-04-29 00:15:29 +0800

    Update scaffold.dart
    scaffold use base platform widget

--- lib/src/scaffold.dart
+++ lib/src/scaffold.dart
@@ -3,27 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'navigationbar.dart';
 import 'platform.dart';
+import 'base_platform_stateless_widget.dart';
 
-class FXScaffold extends StatelessWidget {
-  const FXScaffold({Key key, this.navigationBar, this.content}) : super(key: key);
+class FXScaffold extends BasePlatformStatelessWidget {
+  const FXScaffold({Key key, this.navigationBar, this.content})
+      : super(key: key);
 
   final FXNavigationBar navigationBar;
   final Widget content;
 
   @override
-  Widget build(BuildContext context) {
-     if (FXPlatform.style() == FXAppStyle.material) {
-      return Scaffold(
-          appBar: navigationBar.build(context),
-          body: content,
-          );
-    } else if (FXPlatform.style() == FXAppStyle.cupertino) {
-      return CupertinoPageScaffold(
-        navigationBar: navigationBar.build(context),
-        child: content,
-      );
-    } else {
-      return null;
-    }
+  Widget materialStyleWidget(BuildContext context) {
+    return Scaffold(
+      appBar: navigationBar.build(context),
+      body: content,
+    );
+  }
+
+  @override
+  Widget cupertinoStyleWidget(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: navigationBar.build(context),
+      child: content,
+    );
   }
 }
