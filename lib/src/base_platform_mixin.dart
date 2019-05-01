96df51b87c51c40c1e62361f4ae9551b8ed77672
ZengyiMa <m.zengyi@gmail.com>
2019-05-01 18:36:37 +0800

    Create base_platform_mixin.dart

---
+++ lib/src/base_platform_mixin.dart
@@ -0,0 +1,23 @@
+import 'platform.dart';
+import 'package:flutter/widgets.dart';
+
+class BasePlatformMixin  {
+  
+  Widget build(BuildContext context) {
+    if (XPlatform.style() == XAppStyle.material) {
+      return materialStyleWidget(context);
+    } else if (XPlatform.style() == XAppStyle.cupertino) {
+      return cupertinoStyleWidget(context);
+    } else {
+      return null;
+    }
+  }
+
+  Widget cupertinoStyleWidget(BuildContext context) {
+    return null;
+  }
+
+  Widget materialStyleWidget(BuildContext context) {
+    return null;
+  }
+}
