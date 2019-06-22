f943dc2f01c7c3d4da6360a18b923d3ace83504b
ZengyiMa <m.zengyi@gmail.com>
2019-06-23 01:02:22 +0800

    Create button.dart

---
+++ lib/src/button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'mixin/platform_mixin.dart';
+
+class XButton extends StatelessWidget with PlatformMixin {
+  XButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
