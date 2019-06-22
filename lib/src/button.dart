8c330500567bdc81a0d8334c1630ffaaf5851700
ZengyiMa <m.zengyi@gmail.com>
2019-06-23 01:20:25 +0800

    Update button.dart

--- lib/src/button.dart
+++ lib/src/button.dart
@@ -5,10 +5,21 @@ import 'mixin/platform_mixin.dart';
 class XButton extends StatelessWidget with PlatformMixin {
   XButton({
     Key key,
+    @required this.child,
+    @required this.onPressed,
   }) : super(key: key);
 
+  final Widget child;
+  final VoidCallback onPressed;
+
+
   @override
-  Widget build(BuildContext context) {
-    return null;
+  Widget cupertinoStyleWidget(BuildContext context) {
+    return CupertinoButton(child: child, onPressed: this.onPressed,);
+  }
+
+  @override
+  Widget materialStyleWidget(BuildContext context) {
+    return RaisedButton(child: this.child, onPressed: this.onPressed);
   }
 }
