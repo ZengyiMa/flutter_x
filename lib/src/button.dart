a9bfdeaf7cb48156e08795fd1ca5c1545578743c
ZengyiMa <m.zengyi@gmail.com>
2019-06-23 12:44:54 +0800

    Update button.dart

--- lib/src/button.dart
+++ lib/src/button.dart
@@ -3,30 +3,40 @@ import 'package:flutter/cupertino.dart';
 import 'mixin/platform_mixin.dart';
 
 class XButton extends StatelessWidget with PlatformMixin {
-  XButton({
-    Key key,
-    @required this.child,
-    @required this.onPressed,
-    this.color,
-    this.disabledColor
-  }) : super(key: key);
+  XButton(
+      {Key key,
+      @required this.child,
+      @required this.onPressed,
+      this.color,
+      this.disabledColor,
+      this.padding})
+      : super(key: key);
 
   final Widget child;
   final VoidCallback onPressed;
   final Color color;
   final Color disabledColor;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget cupertinoStyleWidget(BuildContext context) {
     return CupertinoButton(
-        child: child,
-        onPressed: onPressed,
-        color: color,
-        disabledColor: disabledColor);
+      child: child,
+      onPressed: onPressed,
+      color: color,
+      disabledColor: disabledColor,
+      padding: padding,
+    );
   }
 
   @override
   Widget materialStyleWidget(BuildContext context) {
-    return RaisedButton(child: this.child, onPressed: this.onPressed);
+    return RaisedButton(
+      child: child,
+      onPressed: onPressed,
+      color: color,
+      disabledColor: disabledColor,
+      padding: padding,
+    );
   }
 }
