6d68efb579544f44ea3a64851a7255a330d80bb9
ZengyiMa <m.zengyi@gmail.com>
2019-06-28 01:22:21 +0800

    Update flutter_x.dart

--- lib/flutter_x.dart
+++ lib/flutter_x.dart
@@ -2,6 +2,7 @@ library flutter_x;
 
 export 'package:flutter/widgets.dart';
 export 'package:flutter/foundation.dart';
+export 'src/platform_data_provider.dart';
 export 'src/app.dart';
 export 'src/app_theme.dart';
 export 'src/scaffold.dart';
