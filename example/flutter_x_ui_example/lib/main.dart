4a933dcb0d4b6901a93ca33cf467c07e427dacea
ZengyiMa <m.zengyi@gmail.com>
2019-12-23 22:59:58 +0800

    Update main.dart

--- example/flutter_x_ui_example/lib/main.dart
+++ example/flutter_x_ui_example/lib/main.dart
@@ -32,47 +32,59 @@ class HomePage extends StatelessWidget {
     {"title": "Switch", "pageName": "switch_example"},
     {"title": "Button", "pageName": "button_example"},
   ];
-
   @override
   Widget build(BuildContext context) {
     return XScaffold(
-        navigationBar: XNavigationBar(title: "FlutterX Widget Example"),
+        navigationBar: XNavigationBar(title: "Flutter-x UI Widget Example"),
         content: ListView(
           shrinkWrap: true,
-          padding: const EdgeInsets.fromLTRB(16, 88, 16, 64),
-          children: <Widget>[
-            Container(
-                padding: EdgeInsets.all(40),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Flutter X UI',
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.normal,
-                              color: Color(0xff000000))),
-                      Container(
-                          margin: EdgeInsets.only(top: 4),
-                          child: Text(
-                              'Flutter-x UI 基于 Flutter 多端一致性的 UI 框架，一份代码在多个平台上带来一致性又具有平台特色的体验',
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300,
-                                  color: Color(0x80000000))))
-                    ])),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, "switch_example");
-              },
+          padding: const EdgeInsets.fromLTRB(0, 88, 0, 64),
+          children: this.getWidgets(context),
+        ));
+  }
+
+  List<Widget> getWidgets(BuildContext context) {
+    List<Widget> list = List<Widget>();
+    list.add(Container(
+        padding: EdgeInsets.all(40),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Flutter X UI',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Color(0xff000000))),
+              Container(
+                  margin: EdgeInsets.only(top: 4),
+                  child: Text(
+                      'Flutter-x UI 基于 Flutter 多端一致性的 UI 框架，一份代码在多个平台上带来一致性又具有平台特色的体验',
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                          color: Color(0x80000000))))
+            ])));
+    for (var item in demo) {
+      list.add(
+        GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, item["pageName"]);
+            },
+            child: Container(
+              margin: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              height: 44,
+              decoration: BoxDecoration(
+                  border: Border(
+                bottom: BorderSide(width: 1.0, color: Color(0xFFC7C7CC)),
+              )),
               child: Row(
+                mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Container(
-                    padding: EdgeInsets.fromLTRB(16, 10, 16, 5),
-                    child: Text("NavigationBar"),
+                    child: Text(item["title"]),
                   ),
                   Container(
-                    padding: EdgeInsets.fromLTRB(0, 10, 8, 5),
                     child: Icon(
                       CupertinoIcons.forward,
                       color: Color(0xFFC7C7CC),
@@ -81,8 +93,9 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-          ],
-        ));
+            )),
+      );
+    }
+    return list;
   }
 }
